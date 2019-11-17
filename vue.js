4f4e44bb12b699ee7a01442db05e60496f39b2c7
oliyidepaul <oliyidepaul@gmail.com>
2019-11-17 04:12:59 -0800

    Changed vue.js

---
+++ vue.js
@@ -0,0 +1,44 @@
+var x = [[1,2,4], [3,4,5], [5,6,7], [6,7,8]];
+
+// function convert(m) {
+//     for (let i = 0; i < x.length; i++){ 
+//         for (j= 0; x[i].length; j++)
+//         {
+//             if (x[i] > 2) {
+//                 return 3
+//             } else
+//                 return x[i]
+//             }
+                         
+//          }
+
+        
+//     }
+    
+
+
+
+// var y = convert(2) ;
+
+// console.log(y)
+
+// function matrix_multi(a1,a2) {
+//    for (let index = 0; index < array.length; index++) {
+//     for (var j = 0; j < x[i].length; j++) {
+        
+        
+//     }   
+
+//     }   
+    
+// }
+
+
+var r;
+function AreaOfACircle(r) {
+    var a = 22/7 *(r*r);
+     return a;
+    
+}
+
+var b = AreaOfACircle(r)/2;
